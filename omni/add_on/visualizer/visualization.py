697759db2921028460ecb3c35063eebd4b18d36d
Antonio Serrano Muñoz <toni.sm91@gmail.com>
2021-01-21 23:01:21 +0000

    Displays charts using a Matplotlib.pyplot-like interface

--- omni/add_on/visualizer/visualization.py
+++ omni/add_on/visualizer/visualization.py
@@ -1,5 +1,9 @@
 import omni.ui as ui
 
+import numpy as np
+from matplotlib.figure import Figure as Matplotlib_Figure
+from matplotlib.backends.backend_agg import FigureCanvasAgg as Matplotlib_FigureCanvasAgg
+
 
 class Figure():
     def __init__(self, num=None, figsize=(6.4, 4.8), dpi=100.0, **kwargs):
@@ -38,7 +42,17 @@ class Figure():
                                  visible=True)
 
         self._num = num
+        self._dpi = dpi
+        self._figsize = figsize
         self._byte_provider = None
+    
+    def is_instance(self, num):
+        """
+
+        """
+        if num == None:
+            return False
+        return self._num == num
         
     def imshow(self, winname, mat):
         """
@@ -74,10 +88,22 @@ class Figure():
             with ui.VStack():
                 ui.ImageWithProvider(self._byte_provider)
         
-    def is_instance(self, num):
-        if num == None:
-            return False
-        return self._num == num
+    def _render(self, *args, plot_method="plot", **kwargs):
+        # create canvas
+        fig = Matplotlib_Figure(figsize=self._figsize, dpi=self._dpi)
+        canvas = Matplotlib_FigureCanvasAgg(fig)
+        
+        # plotting
+        ax = fig.gca()
+        exec("ax.{}(*args, **kwargs)".format(plot_method))
+
+        # convert renderer buffer to numpy
+        canvas.draw()
+        img = np.asarray(canvas.buffer_rgba())
+        
+        # show rendered image
+        self.imshow(None, img)
+
 
 class Visualization():
     def __init__(self):
@@ -124,5 +150,54 @@ class Visualization():
         """
         self.figure(winname).imshow(winname, mat)
 
+    # matplotib pyplot methods
+    # https://matplotlib.org/3.3.3/api/_as_gen/matplotlib.pyplot.html
+    def acorr(self, *args, plot_method="acorr", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def angle_spectrum(self, *args, plot_method="angle_spectrum", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def bar(self, *args, plot_method="bar", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def barbs(self, *args, plot_method="barbs", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def barh(self, *args, plot_method="barh", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def boxplot(self, *args, plot_method="boxplot", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def broken_barh(self, *args, plot_method="broken_barh", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def cohere(self, *args, plot_method="cohere", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def contour(self, *args, plot_method="contour", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def contourf(self, *args, plot_method="contourf", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def csd(self, *args, plot_method="csd", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def errorbar(self, *args, plot_method="errorbar", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def eventplot(self, *args, plot_method="eventplot", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def fill(self, *args, plot_method="fill", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def fill_between(self, *args, plot_method="fill_between", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def fill_betweenx(self, *args, plot_method="fill_betweenx", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def hist(self, *args, plot_method="hist", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def hist2d(self, *args, plot_method="hist2d", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def hlines(self, *args, plot_method="hlines", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def loglog(self, *args, plot_method="loglog", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def magnitude_spectrum(self, *args, plot_method="magnitude_spectrum", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def matshow(self, *args, plot_method="matshow", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def pcolor(self, *args, plot_method="pcolor", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def pcolormesh(self, *args, plot_method="pcolormesh", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def phase_spectrum(self, *args, plot_method="phase_spectrum", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def pie(self, *args, plot_method="pie", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def plot(self, *args, plot_method="plot", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def plot_date(self, *args, plot_method="plot_date", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def polar(self, *args, plot_method="polar", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def psd(self, *args, plot_method="psd", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def quiver(self, *args, plot_method="quiver", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def scatter(self, *args, plot_method="scatter", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def semilogx(self, *args, plot_method="semilogx", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def semilogy(self, *args, plot_method="semilogy", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def specgram(self, *args, plot_method="specgram", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def spy(self, *args, plot_method="spy", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def stackplot(self, *args, plot_method="stackplot", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def stem(self, *args, plot_method="stem", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def step(self, *args, plot_method="step", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def streamplot(self, *args, plot_method="streamplot", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def tricontour(self, *args, plot_method="tricontour", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def tricontourf(self, *args, plot_method="tricontourf", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def tripcolor(self, *args, plot_method="tripcolor", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def triplot(self, *args, plot_method="triplot", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def violinplot(self, *args, plot_method="violinplot", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    def vlines(self, *args, plot_method="vlines", **kwargs): self.figure(None)._render(*args, plot_method=plot_method, **kwargs)
+    
 
 _visualization = Visualization()
