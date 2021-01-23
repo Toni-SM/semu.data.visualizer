0831ece1777eb1457ad22bbbf89bf72dc2ebf93a
Antonio Serrano Muñoz <toni.sm91@gmail.com>
2021-01-23 11:01:54 +0000

    Create and display native figures

--- omni/add_on/visualizer/visualization.py
+++ omni/add_on/visualizer/visualization.py
@@ -1,21 +1,26 @@
-import omni.ui as ui
-
+import datetime
+import collections
 import numpy as np
+import matplotlib
 from matplotlib.figure import Figure as Matplotlib_Figure
 from matplotlib.backends.backend_agg import FigureCanvasAgg as Matplotlib_FigureCanvasAgg
 
+import omni.ui as ui
 
-class Figure():
-    def __init__(self, num=None, figsize=(6.4, 4.8), dpi=100.0, **kwargs):
+
+class AbstactFigure():
+    def __init__(self, prefix, num=None, figsize=(6.4, 4.8), dpi=100.0, **kwargs):
         """
-        Figure
+        Base class for figures
 
         Based on matplotlib.figure.Figure
         https://matplotlib.org/3.3.3/api/_as_gen/matplotlib.figure.Figure.html?highlight=figure#matplotlib.figure.Figure
 
         Parameters
         ----------
-        num : int or str, optional
+        prefix: str
+            Figure name prefix 
+        num : int, float or str (optional)
             Unique identifier for the figure.
             If the parameter is not given, a new identifier is created based on the current system time
         figsize : tuple
@@ -25,27 +30,24 @@ class Figure():
         """
         # window name
         if num == None:
-            import datetime
             num = str(datetime.datetime.today().time())
-            name = "Figure " + num
-        if type(num) == int:
-            name = "Figure " + str(num)
+        elif type(num) == int or type(num) == float:
+            num = str(num)
         elif type(num) == str:
-            name = num
+            pass
         else:
-            raise Exception("Invalid identifier type for the figure: ")
-        
-        # create window
-        self._window = ui.Window(name,
-                                 width=int(figsize[0]*dpi), 
-                                 height=int(figsize[1]*dpi), 
-                                 visible=True)
+            raise Exception(f"Invalid identifier type for the figure: {str(type(num))}")
 
         self._num = num
         self._dpi = dpi
         self._figsize = figsize
-        self._byte_provider = None
-    
+
+        # create window
+        self._window = ui.Window(f"{prefix}: {self._num}",
+                                 width=int(self._figsize[0]*self._dpi), 
+                                 height=int(self._figsize[1]*self._dpi), 
+                                 visible=True)
+
     def is_instance(self, num):
         """
         Identify if the current figure has the specified identifier
@@ -63,6 +65,25 @@ class Figure():
         if num == None:
             return False
         return self._num == num
+
+
+class Figure(AbstactFigure):
+    def __init__(self, num=None, figsize=(6.4, 4.8), dpi=100.0, **kwargs):
+        """
+        Create Matplotlib and OpenCV based visualizations
+
+        Parameters
+        ----------
+        num : int or str, optional
+            Unique identifier for the figure.
+            If the parameter is not given, a new identifier is created based on the current system time
+        figsize : tuple
+            Width and height of the window in inches
+        dpi: float
+            Resolution of the figure in pixels-per-inch (called dpi to keep compatibility with matplotlib)
+        """
+        super().__init__("Figure", num, figsize, dpi, **kwargs)
+        self._byte_provider = None
         
     def imshow(self, winname, mat):
         """
@@ -176,7 +197,150 @@ class Figure():
     def vlines(self, *args, **kwargs): self._render(*args, plot_method="vlines", **kwargs)
 
 
-class Visualization():
+class NativeFigure(AbstactFigure):
+
+    class NativePlot():
+        def __init__(self, title="", lines=1, colors=[], xlim=None, ylim=None, window_size=250, theme="light"):
+            self._title = title
+            self._lines = lines
+            self._colors = colors
+            self._xlim = xlim
+            self._ylim = ylim
+
+            self._plots = []
+            self._plots_data = []
+
+            for line in range(self._lines):
+                if not window_size or window_size <= 0:
+                    data = [0.0]
+                else:
+                    data = collections.deque([0.0] * window_size, maxlen=window_size)
+                self._plots_data.append(data)
+
+            # theme
+            self._theme = 0xFF555555 if theme == "dark" else 0xFFDDDDDD 
+
+            # colors 
+            if len(self._colors) == self._lines:
+                self._colors = self._generate_lines_color(self._lines, self._colors)            
+            else:
+                self._colors = self._generate_lines_color(self._lines)            
+
+        def _generate_lines_color(self, amout, colors=None):
+            if not colors:
+                x11_css4_color_names = ['red', 'green', 'blue', 'orange', 'cyan', 'magenta', 'yellow', 'white', 'aliceblue', 'antiquewhite', 'aqua', 'aquamarine', 'azure', 'beige', 'bisque', 'black', 'blanchedalmond', 'blueviolet', 'brown', 'burlywood', 'cadetblue', 'chartreuse', 'chocolate', 'coral', 'cornflowerblue', 'cornsilk', 'crimson', 'darkblue', 'darkcyan', 'darkgoldenrod', 'darkgray', 'darkgreen', 'darkgrey', 'darkkhaki', 'darkmagenta', 'darkolivegreen', 'darkorange', 'darkorchid', 'darkred', 'darksalmon', 'darkseagreen', 'darkslateblue', 'darkslategray', 'darkslategrey', 'darkturquoise', 'darkviolet', 'deeppink', 'deepskyblue', 'dimgray', 'dimgrey', 'dodgerblue', 'firebrick', 'floralwhite', 'forestgreen', 'fuchsia', 'gainsboro', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'greenyellow', 'grey', 'honeydew', 'hotpink', 'indianred', 'indigo', 'ivory', 'khaki', 'lavender', 'lavenderblush', 'lawngreen', 'lemonchiffon', 'lightblue', 'lightcoral', 'lightcyan', 'lightgoldenrodyellow', 'lightgray', 'lightgreen', 'lightgrey', 'lightpink', 'lightsalmon', 'lightseagreen', 'lightskyblue', 'lightslategray', 'lightslategrey', 'lightsteelblue', 'lightyellow', 'lime', 'limegreen', 'linen', 'maroon', 'mediumaquamarine', 'mediumblue', 'mediumorchid', 'mediumpurple', 'mediumseagreen', 'mediumslateblue', 'mediumspringgreen', 'mediumturquoise', 'mediumvioletred', 'midnightblue', 'mintcream', 'mistyrose', 'moccasin', 'navajowhite', 'navy', 'oldlace', 'olive', 'olivedrab', 'orangered', 'orchid', 'palegoldenrod', 'palegreen', 'paleturquoise', 'palevioletred', 'papayawhip', 'peachpuff', 'peru', 'pink', 'plum', 'powderblue', 'purple', 'rebeccapurple', 'rosybrown', 'royalblue', 'saddlebrown', 'salmon', 'sandybrown', 'seagreen', 'seashell', 'sienna', 'silver', 'skyblue', 'slateblue', 'slategray', 'slategrey', 'snow', 'springgreen', 'steelblue', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'wheat', 'whitesmoke', 'yellowgreen']
+                colors = x11_css4_color_names[:amout]
+            colors = [matplotlib.colors.to_hex(color, keep_alpha=False).upper()[1:] for color in colors]
+            return [int(f"0xFF{color[-2:]}{color[2:-2]}{color[:2]}", 0) for color in colors]
+
+        def draw(self):
+            ylim = self._ylim
+            if not ylim:
+                ylim = (-1, 1)
+            
+            # title
+            if self._title:
+                ui.Label(self._title, width=0, alignment=ui.Alignment.LEFT_TOP)
+                ui.Spacer(height=5)
+            # label: max
+            self._label_max = ui.Label(str(ylim[1]), width=0, alignment=ui.Alignment.LEFT_TOP)
+            # plots
+            with ui.ZStack():
+                self._plots = []
+                ui.Rectangle(style={"background_color": self._theme, "border_color": 0xFF000000, "border_width": 1})
+                with ui.Frame(width=ui.Percent(100), height=100, opaque_for_mouse_events=True, style={"color": 0xFFFFFFFF}):
+                    with ui.ZStack():
+                        for line in range(self._lines):
+                            self._plots.append(ui.Plot(ui.Type.LINE, 
+                                            ylim[0], ylim[1], 
+                                            *self._plots_data[line],
+                                            width=ui.Percent(100),
+                                            height=100,
+                                            style={"color": self._colors[line], "background_color": 0x00000000}))
+            # label: min
+            self._label_min = ui.Label(str(ylim[0]), width=0, alignment=ui.Alignment.LEFT_BOTTOM)
+
+        def add_data(self, data):
+            # add new data
+            if type(data) == int or type(data) == float:
+                self._plots_data[0].append(data)
+            else:
+                for line in range(self._lines):
+                    self._plots_data[line].append(data[line])
+            # update plots
+            if not self._ylim:
+                ylim = [0, 0]
+                ylim[0] = min([min(self._plots_data[line]) for line in range(self._lines)])
+                ylim[1] = max([max(self._plots_data[line]) for line in range(self._lines)])
+                self._label_min.text = f"{round(ylim[0], 3)}"
+                self._label_max.text = f"{round(ylim[1], 3)}"
+            for i in range(len(self._plots)):
+                if not self._ylim:
+                    self._plots[i].scale_min = ylim[0]
+                    self._plots[i].scale_max = ylim[1]
+                self._plots[i].set_data(*self._plots_data[i])
+        
+        def set_data(self, data):
+            # validate data
+            if len(data) != self._lines:
+                raise Exception("Invalid data length")
+            # set new data
+            for line in range(self._lines):
+                if type(data[line]) == np.ndarray:
+                    self._plots_data[line]=data[line].tolist()
+                else:
+                    self._plots_data[line]=data[line]
+            # update plots
+            if not self._ylim:
+                ylim = [0, 0]
+                ylim[0] = min([min(self._plots_data[line]) for line in range(self._lines)])
+                ylim[1] = max([max(self._plots_data[line]) for line in range(self._lines)])
+                self._label_min.text = f"{round(ylim[0], 3)}"
+                self._label_max.text = f"{round(ylim[1], 3)}"
+            for i in range(len(self._plots)):
+                if not self._ylim:
+                    self._plots[i].scale_min = ylim[0]
+                    self._plots[i].scale_max = ylim[1]
+                self._plots[i].set_data(*self._plots_data[i])
+    
+    def __init__(self, num=None, figsize=(6.4, 4.8), dpi=100.0, **kwargs):
+        """
+        Create native Omniverse visualizations
+
+        Parameters
+        ----------
+        num : int or str, optional
+            Unique identifier for the figure.
+            If the parameter is not given, a new identifier is created based on the current system time
+        figsize : tuple
+            Width and height of the window in inches
+        dpi: float
+            Resolution of the figure in pixels-per-inch (called dpi to keep compatibility with matplotlib)
+        """
+        super().__init__("Native figure", num, figsize, dpi, **kwargs)
+        self._native_plots = {}
+
+    def __getitem__(self, index):
+        return self._native_plots[index]
+
+    def add_plot(self, title="", lines=1, colors=[], xlim=None, ylim=None, window_size=250, theme="light", id=None):
+        if not id:
+            id = str(datetime.datetime.today().time())
+        self._native_plots[id] = self.NativePlot(title=title, 
+                                                 lines=lines, colors=colors, 
+                                                 xlim=xlim, ylim=ylim, 
+                                                 window_size=window_size, 
+                                                 theme=theme)
+        # draw ui
+        with self._window.frame:
+            with ui.VStack(height=0):
+                for k in self._native_plots:
+                    self._native_plots[k].draw()
+                    ui.Spacer(height=15)
+        return self._native_plots[id]
+
+
+class Visualizer():
     def __init__(self):
         """
         Data visualization for NVIDIA Omniverse
@@ -185,6 +349,7 @@ class Visualization():
         """
         # TODO: release figures after close them
         self._figures = []
+        self._native_figures = []
 
     def figure(self, num=None, figsize=(6.4, 4.8), dpi=100.0, **kwargs):
         """
@@ -210,6 +375,32 @@ class Visualization():
                 return fig
         fig = Figure(num, figsize, dpi, **kwargs)
         self._figures.append(fig)
+        return fig
+
+    def native_figure(self, num=None, figsize=(6.4, 4.8), dpi=100.0, **kwargs):
+        """
+        Create a new native figure or return an existing one
+
+        Parameters
+        ----------
+        num : int or str, optional
+            Unique identifier for the figure.
+            If the parameter is not given, a new identifier is created based on the current system time
+        figsize : tuple
+            Width and height of the window in inches
+        dpi: float
+            Resolution of the figure in pixels-per-inch (called dpi to keep compatibility with matplotlib)
+        
+        Returns
+        -------
+        Figure
+            Instance of the created or existing native figure
+        """
+        for fig in self._native_figures:
+            if fig.is_instance(num):
+                return fig
+        fig = NativeFigure(num, figsize, dpi, **kwargs)
+        self._native_figures.append(fig)
         return fig
 
     def imshow(self, winname, mat):
@@ -274,4 +465,4 @@ class Visualization():
     def vlines(self, *args, **kwargs): self.figure(None).vlines(*args, **kwargs)
     
 
-_visualization = Visualization()
+_visualizer = Visualizer()
