42482ecf7cfecddc65b3dca77d70070b4c071c00
Antonio Serrano MUñoz <toni.semu@gmail.com>
2022-03-26 08:14:10 +0100

    Use os module functions to define file paths

--- compile.py
+++ compile.py
@@ -1,13 +1,19 @@
+import os
 from distutils.core import setup
 from distutils.extension import Extension
 
 from Cython.Distutils import build_ext
 
 
+# OV python (kit\python\include)
+python_library_dir = os.path.join("..", "..", "kit", "python", "include")
+if not os.path.exists(python_library_dir):
+    python_library_dir = os.path.join("app", os.path.join("..", "..", "app", "kit", "python", "include"))
+
 ext_modules = [
     Extension("_data_visualizer",
-              ["omni/add_on/visualizer/data_visualizer.py"],
-              library_dirs=['/isaac-sim/kit/python/include']),
+              [os.path.join("omni", "add_on", "visualizer", "data_visualizer.py")],
+              library_dirs=[python_library_dir]),
 ]
 
 setup(
