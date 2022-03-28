0ca1b7124701cb4a78e7186f4bd6d728fe1991d2
Antonio Serrano MUñoz <toni.semu@gmail.com>
2022-03-28 19:40:43 +0200

    Get python path from sys module

--- src/omni.add_on.visualizer/compile_extension.py
+++ src/omni.add_on.visualizer/compile_extension.py
@@ -1,14 +1,18 @@
 import os
+import sys
 from distutils.core import setup
 from distutils.extension import Extension
 
 from Cython.Distutils import build_ext
 
-
 # OV python (kit\python\include)
-python_library_dir = os.path.join("..", "..", "kit", "python", "include")
+if sys.platform == 'win32':
+    python_library_dir = os.path.join(os.path.dirname(sys.executable), "include")
+elif sys.platform == 'linux':
+    python_library_dir = os.path.join(os.path.dirname(sys.executable), "..", "include")
+
 if not os.path.exists(python_library_dir):
-    python_library_dir = os.path.join("app", os.path.join("..", "..", "app", "kit", "python", "include"))
+    raise Exception("OV Python library directory not found: {}".format(python_library_dir))
 
 ext_modules = [
     Extension("_visualizer",
